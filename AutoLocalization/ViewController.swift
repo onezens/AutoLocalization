abf0f9980d52fa33f6fb108648c46e3237f61c42
wackosix <mail@wackosix.cn>
2016-04-07 23:27:17 +0800

    init project & upload project

---
+++ AutoLocalization/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AutoLocalization
+//
+//  Created by WackoSix on 4/7/16.
+//  Copyright © 2016 www.wackosix.cn. All rights reserved.
+//  ForStoryboardandXib
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
